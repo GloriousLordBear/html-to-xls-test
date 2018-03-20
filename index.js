d90758bf72520199626f591e5ae07d0912506e93
Arthur Ladislau Pereira <arthur.pereira@triggolabs.com>
2018-03-20 17:21:28 -0300

    WORKS!

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 const http = require('http');
-const convert = require("html-to-xlsx")();
+const conversion = require("html-to-xlsx")();
 const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
-const server = http.createServer((request, response) => {
-  request.on('error', (err) => {
+const server = http.createServer((req, res) => {
+  req.on('error', (err) => {
     console.error(err);
-    response.statusCode = 400;
-    response.end();
+    res.statusCode = 400;
+    res.end();
   });
-  response.on('error', (err) => {
+  res.on('error', (err) => {
     console.error(err);
   });
-  if (request.method === 'GET' && request.url === '/echo') {
-    request.pipe(response);
+  if (req.method === 'GET' && req.url === '/echo') {
+    var conversion = require("html-to-xlsx")();
     //TableExport("<table class=\"tg\">    <tr>      <th class= \"tg-yw4l\">coiso</th>      <th class=\"tg-yw4l\">troço</th>      <th class=\"tg-yw4l\">lance</th>      <th class=\"tg-yw4l\">esquema</th>    </tr>    <tr>      <td class=\"tg-yw4l\">3</td>      <td class=\"tg-yw4l\">4</td>      <td class=\"tg-yw4l\">5</td>      <td class=\"tg-yw4l\">6</td>    </tr>  </table>");
-    convert("<table class=\"tg\"><tr><th class= \"tg-yw4l\">coiso</th><th class=\"tg-yw4l\">troço</th><th class=\"tg-yw4l\">lance</th><th class=\"tg-yw4l\">esquema</th></tr><tr><td class=\"tg-yw4l\">3</td><td class=\"tg-yw4l\">4</td><td class=\"tg-yw4l\">5</td><td class=\"tg-yw4l\">6</td></tr></table>",  function(err, stream){
-        //readable stream to xlsx file
-        stream.pipe(res);
-      });  
+    conversion("<table><tr><td>cell value</td></tr></table>", function(err, stream){
+    //readable stream to xlsx file
+    console.log("yay");
+    stream.pipe(res);
+    }); 
   } else {
-    response.statusCode = 404;
-    response.end();
+    res.statusCode = 404;
+    res.end();
   }
 });
 
